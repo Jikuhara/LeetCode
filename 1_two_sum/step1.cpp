d3c50c56ee9eb587bf46cba2559fa6975d435205
Jikuhara <kei2003730@gmail.com>
2025-06-11 17:43:11 +0900

    1. Two Sum
    https://leetcode.com/problems/two-sum/description/

---
+++ 1_two_sum/step1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+	std::unordered_map<int, int> num_to_index;
+
+	for (int i = 0; i < (int)nums.size(); ++i) {
+		int complement = target - nums[i];
+		if (num_to_index.find(complement) != num_to_index.end()) {
+			return {num_to_index[complement], i};
+		}
+		num_to_index[nums[i]] = i;
+	}
+	return {};
+}
+
+int main() {
+	std::vector<int> nums = {2, 7, 11, 15};
+	int target = 9;
+
+	std::vector<int> result = twoSum(nums, target);
+	std::cout << "Indices : " << result[0] << ", " << result[1] << std::endl;
+
+	return 0;
+}
